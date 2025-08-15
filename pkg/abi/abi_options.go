940fb769981f3765352bac47656abc990fdbd9e0
Rio Martinez <rio@riomartinez.com>
2025-08-15 18:59:02 +0200

    chore: clean up AbiOptions interface

--- pkg/abi/abi_options.go
+++ pkg/abi/abi_options.go
@@ -2,8 +2,6 @@ package abi
 
 import (
 	"context"
-
-	"github.com/tetratelabs/wazero/api"
 )
 
 type StringEncoding string
@@ -35,27 +33,19 @@ func (e StringEncoding) Alignment() uint32 {
 	}
 }
 
+type RuntimeCall func(ctx context.Context, name string, params ...uint64) ([]uint64, error)
+
 type RuntimeMemory interface {
 	Size() uint32
-	ReadByte(offset uint32) (byte, bool)
-	ReadUint16Le(offset uint32) (uint16, bool)
-	ReadUint32Le(offset uint32) (uint32, bool)
-	ReadFloat32Le(offset uint32) (float32, bool)
-	ReadUint64Le(offset uint32) (uint64, bool)
-	ReadFloat64Le(offset uint32) (float64, bool)
 	Read(offset, byteCount uint32) ([]byte, bool)
-	WriteByte(offset uint32, v byte) bool
-	WriteUint16Le(offset uint32, v uint16) bool
-	WriteUint32Le(offset, v uint32) bool
-	WriteFloat32Le(offset uint32, v float32) bool
-	WriteUint64Le(offset uint32, v uint64) bool
-	WriteFloat64Le(offset uint32, v float64) bool
+	ReadUint32Le(offset uint32) (uint32, bool)
 	Write(offset uint32, v []byte) bool
+	WriteUint32Le(offset, v uint32) bool
 }
 
 type AbiOptions struct {
 	StringEncoding StringEncoding
-	Context        context.Context
 	Memory         RuntimeMemory
-	Func           func(name string) api.Function
+	Call           RuntimeCall
+	Context        context.Context
 }
--- pkg/abi/function.go
+++ pkg/abi/function.go
@@ -5,14 +5,10 @@ import (
 )
 
 func Call(opts AbiOptions, name string, params ...uint64) (ret uint32, postReturn func() error, err error) {
-	if opts.Func == nil {
-		return 0, nil, fmt.Errorf("module is not set in AbiOptions")
+	if opts.Call == nil {
+		return 0, nil, fmt.Errorf("call function is not defined in AbiOptions")
 	}
-	fn := opts.Func(name)
-	if fn == nil {
-		return 0, nil, fmt.Errorf("function %s not found in module", name)
-	}
-	results, err := fn.Call(opts.Context, params...)
+	results, err := opts.Call(opts.Context, name, params...)
 	if err != nil {
 		return 0, nil, fmt.Errorf("failed to call function %s: %w", name, err)
 	}
@@ -20,12 +16,8 @@ func Call(opts AbiOptions, name string, params ...uint64) (ret uint32, postRetur
 		ret = uint32(results[0])
 	}
 	postReturn = func() error {
-		postFn := opts.Func("cabi_post_" + name)
-		if postFn != nil {
-			_, err := postFn.Call(opts.Context, uint64(ret))
-			return err
-		}
-		return nil
+		_, err := opts.Call(opts.Context, "cabi_post_"+name, uint64(ret))
+		return err
 	}
 	return ret, postReturn, err
 }
